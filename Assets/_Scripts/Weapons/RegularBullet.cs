cfe54c8d7ce91b0f7cdd270bb2cc5e835e022fdb
Mark Steffensen <crazyeight68@gmail.com>
2021-06-16 22:52:13 -0700

    Projectile Collision
    Added basic projectile collision for the regular bullet type.

--- Assets/_Scripts/Weapons/RegularBullet.cs
+++ Assets/_Scripts/Weapons/RegularBullet.cs
@@ -22,4 +22,19 @@ public class RegularBullet : Bullet
         rigidbody2d.MovePosition(transform.position + BulletData.BulletSpeed * transform.right * Time.deltaTime);    
     }
 
+    private void OnTriggerEnter2D(Collider2D collision) 
+    {
+        Debug.Log(collision.gameObject.name);
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Obstacle"))
+        {
+            HitObstacle();
+        }
+
+        Destroy(gameObject);
+    }
+
+    private void HitObstacle()
+    {
+        Debug.Log("Hitting obstacle."); //TODO: Need to implement proper collision detection
+    }
 }
