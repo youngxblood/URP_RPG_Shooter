5f2f3bf2df2a88f678d678ba0f9167d20ce3af9b
Mark Steffensen <crazyeight68@gmail.com>
2021-06-17 18:58:18 -0700

    Weapon fire and out of ammo SFX
    Implemented a more flexible audio system using an abstract class.

--- Assets/_Scripts/AgentStepAudioPlayer.cs
+++ Assets/_Scripts/AgentStepAudioPlayer.cs
@@ -2,41 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(AudioSource))]
-public class AgentStepAudioPlayer : MonoBehaviour
+public class AgentStepAudioPlayer : AudioPlayer
 {
-    protected AudioSource audioSource;
-    [SerializeField]
-    protected float pitchVariance = 0.05f;
-    protected float basePitch;
 
     [SerializeField]
     protected AudioClip stepClip;
-
-    private void Awake()
-    {
-        audioSource = GetComponent<AudioSource>();
-    }
-
-    private void Start()
-    {
-        //Sets the basePitch to the clips pitch
-        basePitch = audioSource.pitch;
-    }
-
-    protected void PlayClipWithVariablePitch(AudioClip clip)
-    {
-        var randomPitch = UnityEngine.Random.Range(-pitchVariance, pitchVariance);
-        audioSource.pitch = basePitch + randomPitch;
-        PlayClip(clip);
-    }
-
-    protected void PlayClip(AudioClip clip)
-    {
-        audioSource.Stop();
-        audioSource.clip = clip;
-        audioSource.Play();
-    }
 
     public void PlayStepSound()
     {
---
+++ Assets/_Scripts/Weapons/WeaponAudio.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponAudio : AudioPlayer
+{
+    [SerializeField] private AudioClip shootBulletClip, outOfBulletsClip = null;
+
+    public void PlayShootSound()
+    {
+        PlayClip(shootBulletClip);
+    }
+    public void PlayNoBulletsSound()
+    {
+        PlayClip(outOfBulletsClip);
+    }
+}
