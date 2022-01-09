6cd789ddd869a76ef8e9f7e1ae7aab685c344b7e
Mark Steffensen <crazyeight68@gmail.com>
2022-01-09 01:21:05 -0800

    Implemented Warlock's ranged orb attack. Need to have it aim at player.

--- Assets/_Scripts/Enemies/EnemyRangeAttack.cs
+++ Assets/_Scripts/Enemies/EnemyRangeAttack.cs
@@ -4,13 +4,36 @@ using UnityEngine;
 
 public class EnemyRangeAttack : EnemyAttack
 {
+    [SerializeField] protected GameObject muzzle;
+    [SerializeField] protected int ammo;
+    [SerializeField] protected WeaponDataSO weaponData;
+
     public override void Attack(int damage)
     {
-        if(waitBeforeNextAttack == false)
+        if (waitBeforeNextAttack == false)
         {
             var hittable = GetTarget().GetComponent<IHittable>();
-            hittable?.GetHit(damage, gameObject);
+            // hittable?.GetHit(damage, gameObject);
+            ShootBullet();
             StartCoroutine(WaitBeforeAttackCoroutine());
         }
+    }
+
+    private void ShootBullet()
+    {
+        SpawnBullet(muzzle.transform.position, CalculateAngle(muzzle));
+    }
+
+    private void SpawnBullet(Vector3 position, Quaternion rotation)
+    {
+        var bulletPrefab = Instantiate(weaponData.BulletData.bulletPrefab, position, rotation);
+        bulletPrefab.GetComponent<Bullet>().BulletData = weaponData.BulletData;
+    }
+
+    private Quaternion CalculateAngle(GameObject muzzle)
+    {
+        float spread = UnityEngine.Random.Range(-weaponData.SpreadAngle, weaponData.SpreadAngle); // Spread between negative spread value and it's positive form
+        Quaternion bulletSpreadRotation = Quaternion.Euler(new Vector3(0, 0, spread)); // We only want to rotate the bullets on the "Z" angle
+        return muzzle.transform.rotation * bulletSpreadRotation; // Multiplying the muzzle's rotation by bullet spread rotation adds the two values together
     }
 }
