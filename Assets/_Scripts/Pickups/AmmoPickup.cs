1418d98ccf8a6ececb27c990b95161855463d396
Mark Steffensen <crazyeight68@gmail.com>
2022-01-09 16:57:23 -0800

    Added an ammo crate!

---
+++ Assets/_Scripts/Pickups/AmmoPickup.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AmmoPickup : MonoBehaviour
+{
+    [SerializeField] private int pickupAmmoCount = 20;
+    private Weapon playerWeapon;
+    [SerializeField] private UnityEvent ammoPickup;
+    private SpriteRenderer sprite;
+
+    private void Awake() 
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            playerWeapon = collision.GetComponentInChildren<Weapon>();
+
+            if (!playerWeapon.AmmoFull)
+            {
+                playerWeapon.Ammo += pickupAmmoCount;
+                ammoPickup?.Invoke();
+                sprite.enabled = false;
+                StartCoroutine(DestroyObjectCoroutine());
+            }
+        }
+    }
+
+    IEnumerator DestroyObjectCoroutine()
+    {
+        yield return new WaitForSeconds(0.21f);
+        Destroy(gameObject);
+    }
+}
