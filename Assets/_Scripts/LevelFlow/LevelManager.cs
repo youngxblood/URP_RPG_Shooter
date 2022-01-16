a498a37f8a3c165de2ddd4c242b13751c2fa1de1
Mark Steffensen <crazyeight68@gmail.com>
2022-01-15 23:54:05 -0800

    Started on level loading system.

---
+++ Assets/_Scripts/LevelFlow/LevelManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    private Scene currentScene;
+    private Scene nextScene;
+    private Scene[] allScenes;
+    public List<string> scenes;
+
+    private void Awake()
+    {
+        currentScene = SceneManager.GetActiveScene();
+
+        int sceneCount = UnityEngine.SceneManagement.SceneManager.sceneCountInBuildSettings;
+        for (int i = 0; i < sceneCount; i++)
+        {
+            string sceneName = System.IO.Path.GetFileNameWithoutExtension(UnityEngine.SceneManagement.SceneUtility.GetScenePathByBuildIndex(i));
+            scenes.Add(sceneName);
+            Debug.Log("Scene: " + sceneName);
+        }
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
