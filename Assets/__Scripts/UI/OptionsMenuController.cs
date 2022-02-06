725082f07f1ca5b18186f46e49346836e3bb7083
Mark Steffensen <crazyeight68@gmail.com>
2022-02-05 17:42:30 -0800

    Hooked up resolution, fullscreen, graphics quality and audio. Audio doesn't work and settings revert upon returning to main menu.

--- Assets/__Scripts/UI/OptionsMenuController.cs
+++ Assets/__Scripts/UI/OptionsMenuController.cs
@@ -1,11 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class OptionsMenuController : MonoBehaviour
 {
+    // Audio
+    public AudioMixer audioMixer;
+    private string _globalAudioParam = "globalVolume";
+
+    // Graphics
+    private Resolution[] _resolutions;
+    public Dropdown resolutionDropdown;
+
+    private void Start() 
+    {
+        _resolutions = Screen.resolutions; 
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < _resolutions.Length; i++)
+        {
+            string option = _resolutions[i].width + "x" + _resolutions[i].height;
+            options.Add(option);
+
+            if (_resolutions[i].width == Screen.currentResolution.width &&
+                _resolutions[i].height == Screen.currentResolution.height)
+                    currentResolutionIndex = i;
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex; // Set res to current screen resolution of user
+        resolutionDropdown.RefreshShownValue();
+    }
+
     public void SetVolume(float volume)
     {
-        Debug.Log(volume);
+        audioMixer.SetFloat(_globalAudioParam, volume);
+    }
+
+    public void SetGraphicsQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = _resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
